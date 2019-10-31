c2d3e933fcc6f37456f92b0a8befc36c549f5681
mrNicklaus <scriptyacademy@gmail.com>
2019-10-31 04:08:39 +0300

    Ice Cream Complex Conditional Statements

---
+++ practise.js
@@ -0,0 +1,19 @@
+
+
+// change the values of `flavor`, `vessel`, and `toppings` to test your code
+var flavor = "strawberry";
+var vessel = "cone";
+var toppings = "cookies";
+
+
+if (flavor === "vanilla" || "chocolate" && vessel === "cone"|| "bowl" && toppings === "sprinkles" || "peanuts") {
+
+console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings +".");
+
+}
+
+else {
+
+console.log("No ice cream.");
+
+}
